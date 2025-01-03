f3fc08c9f4f60164f26a29753c0dc540f3e1ea1e
Tamanna Anand <anandtamanna9@gmail.com>
2025-01-03 17:26:16 -0400

    Contact Section Works

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -39,22 +39,20 @@
             </div>
                 <div
                 style="background: #1d1e2f; width: 100%; height: 100%; border-radius: 20px; overflow: hidden; border: 1px solid #111a3e; backdrop-filter: blur(9px); -webkit-backdrop-filter: blur(9px);">
-                <form class="flex flex-col p-2" data-aos="zoom-in-up">
-                    <div class="mb-6">
-                        <label for="email" class="text-white block mb-2 text-sm font-medium">Email</label>
-                        <input type="email" id="email" class="bg-[#41436A] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="email@gmail.com" name="email">
-                    </div>
+                <form class="flex flex-col p-2" data-aos="zoom-in-up" action="mailto:anandtamanna9@gmail.com" method="get" enctype="text/plain">
                     <div class="mb-6">
                         <label for="subject" class="text-white block mb-2 text-sm font-medium">Subject</label>
-                        <input type="subject" id="subject" class="bg-[#41436A] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="subject" name="subject">
+                        <input type="text" id="subject" class="bg-[#41436A] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Your Name" name="subject">
+                    </div>
+                    <div class="mb-6">
+                        <label for="email" class="text-white block mb-2 text-sm font-medium">Email</label>
+                        <input type="email" id="email" class="bg-[#41436A] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Your Email" name="email">
                     </div>
                     <div class="mb-6">
                         <label for="message" class="text-white block mb-2 text-sm font-medium">Message</label>
-                        <textarea type="message" id="message" class="bg-[#41436A] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Let's talk about..."name="message"></textarea>
+                        <textarea type="message" id="message" rows="5" cols="30" class="bg-[#41436A] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Let's talk about..."name="body"></textarea>
                     </div>
-                    <button class="z-1 w-[100%!important] px-6 md:px-7 py-3 rounded-full sm:w-max flex justify-center text-white bg-primary border-2 border-transparent">
-                        Send Message
-                    </button>
+                    <input type="submit" value="Send Message" class="z-1 w-[100%!important] px-6 md:px-7 py-3 rounded-full sm:w-max flex justify-center text-white bg-primary border-2 border-transparent">
                 </form>
             </div>
         </div>
